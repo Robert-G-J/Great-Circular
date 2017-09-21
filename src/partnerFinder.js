7273ce78e8256ee7a327994ea02749e28697d8bd
Robert Jones <robert.g.jones@gmail.com>
2017-09-21 17:02:34 +0100

    Change partnerFinder to include argument

--- src/partnerFinder.js
+++ src/partnerFinder.js
@@ -1,11 +1,20 @@
 function PartnerFinder() {
+  this.yourLatitude = null;
+  this.yourLongitude = null;
 }
 
-PartnerFinder.prototype.setYourCoordinates = function() {
+PartnerFinder.prototype.setYourCoordinates = function(coordinates) {
+  var radianCoords = coordinates.map(function(coordinate) {
+    coordinate * (Math.PI/2)
+  });
+  return radianCoords; 
 }
 
 PartnerFinder.prototype.getClosestPartner = function(range) {
-
+  // walks the tree of json picks id and location
+  // calls great circle theorem
+  // compares distance to 100
+  // 
 }
 
 module.exports.PartnerFinder = PartnerFinder;
