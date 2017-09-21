a69641cb6fb82523e14132e30ed1be8ebc04f2ae
Robert Jones <robert.g.jones@gmail.com>
2017-09-21 17:00:00 +0100

    Add Great Circle Distance formula to constructor
    This takes four arguments in a specific order to calculate the distance
    across the surface to a sphere such that it is accurate for large
    distance calculations across the earth's surface.

--- spec/greatCircleSpec.js
+++ spec/greatCircleSpec.js
@@ -4,17 +4,25 @@ describe("Great Circle", function() {
   
   beforeEach(function() {
     gc = new gC.GreatCircle();
+    latBanbury = 52.0629009;
+    lonBanbury = -1.3397750000000315;
   });
   
   it("converts degrees to radians", function() {
-   var lat = 52.0629009;
-   var lon = -1.3397750000000315;  
+   //var lat = 52.0629009;
+   //var lon = -1.3397750000000315;  
    var expected =  [0.9086690388423, -0.023383484985849571636];
-   var radianArray = gc.convertToRadians([lat, lon]);
+   var radianArray = gc.convertToRadians([latBanbury, lonBanbury]);
    radianArray.every((x, i) => expect(x).toBeCloseTo(expected[i]));
   });
   
-  it("calculates the great spherical distance", function() {
+  it("calculates the orthodromic distance", function() {
+    var latLondon = 51.515419;
+    var lonLondon = -0.141099;
+    var radsLondon = gc.convertToRadians([latLondon, lonLondon]);
+    var radsBanbury = gc.convertToRadians([latBanbury, lonBanbury]);
+    var result = gc.orthodromicDistance(radsLondon[0], radsLondon[1], radsBanbury[0], radsBanbury[1]);
+    expect(result).toBeCloseTo(102.48330298105786);
   });
 
 })
--- src/greatCircle.js
+++ src/greatCircle.js
@@ -8,4 +8,9 @@ GreatCircle.prototype.convertToRadians = function(array) {
   return results;
 };
 
+GreatCircle.prototype.orthodromicDistance = function(lat1, long1, lat2, long2) {
+  var meanEarthRadius = 6371000;
+  return (meanEarthRadius*(Math.acos(Math.sin(lat1)* Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(long2 - long1))))/1000
+}
+
 module.exports.GreatCircle = GreatCircle;
